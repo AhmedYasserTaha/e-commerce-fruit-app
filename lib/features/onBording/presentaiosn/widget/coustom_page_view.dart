ccdcf2597db21dd161ffe0f011045bd4ec47e893
Ahmed Yasser <ayt614614@gmail.com>
2025-02-13 16:24:31 +0200

    Finshed dots indicator in bording screen now finshed ondoreding screen

--- lib/features/onBording/presentaiosn/widget/coustom_page_view.dart
+++ lib/features/onBording/presentaiosn/widget/coustom_page_view.dart
@@ -2,23 +2,24 @@ import 'package:e_commerce_app/features/onBording/presentaiosn/widget/page_view_
 import 'package:flutter/material.dart';
 
 class CoustomPageView extends StatelessWidget {
-  const CoustomPageView({super.key});
-
+  const CoustomPageView({super.key, required this.pageController});
+  final PageController? pageController;
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: pageController,
       children: [
         PageViewItem(
             title: "E Shopping",
             suptitle: "Explore top organic fruits & grab then",
             image: "assets/images/onboarding1.png"),
         PageViewItem(
-            title: "E Shopping",
-            suptitle: "Explore top organic fruits & grab then",
+            title: "Delivery in the way",
+            suptitle: "Get your order by speed delivery",
             image: "assets/images/onboarding2.png"),
         PageViewItem(
-            title: "E Shopping",
-            suptitle: "Explore top organic fruits & grab then",
+            title: "Delivery Arrived",
+            suptitle: "Order is arrived at your place",
             image: "assets/images/onboarding3.png"),
       ],
     );
--- lib/features/onBording/presentaiosn/widget/on_bording_body.dart
+++ lib/features/onBording/presentaiosn/widget/on_bording_body.dart
@@ -1,34 +1,71 @@
 import 'package:e_commerce_app/core/utils/app_colors.dart';
 import 'package:e_commerce_app/core/utils/size_config.dart';
 import 'package:e_commerce_app/core/widgets/custom_widget_botton.dart';
+import 'package:e_commerce_app/features/onBording/presentaiosn/widget/coustom_dots_Indicator.dart';
 import 'package:e_commerce_app/features/onBording/presentaiosn/widget/coustom_page_view.dart';
 import 'package:flutter/material.dart';
 
-class OnBordingBody extends StatelessWidget {
+class OnBordingBody extends StatefulWidget {
   const OnBordingBody({super.key});
 
   @override
+  State<OnBordingBody> createState() => _OnBordingBodyState();
+}
+
+class _OnBordingBodyState extends State<OnBordingBody> {
+  PageController? pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: 0)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // ✅ تأكد من تهيئة SizeConfig قبل الاستخدام
+    // تأكد من تهيئة SizeConfig قبل الاستخدام
     SizeConfig().init(context);
 
     return Stack(
       children: [
-        CoustomPageView(),
+        CoustomPageView(
+          pageController: pageController,
+        ),
+        // الثلاث نقاط في الصفحة
         Positioned(
-          top: MediaQuery.of(context).size.height * .1,
-          right: 32,
-          child: Text(
-            "Skip",
-            style: TextStyle(fontSize: 16, color: AppColors.textColor),
+          left: 0,
+          right: 0,
+          bottom: SizeConfig.defaultSize! * 20,
+          child: CoustomDotsIndicator(
+            dontIndex: (pageController?.hasClients ?? false)
+                ? pageController!.page ?? 0
+                : 0,
+          ),
+        ),
+        Visibility(
+          visible: (pageController?.hasClients ?? false) &&
+              (pageController?.page?.round() == 2 ? false : true),
+          child: Positioned(
+            top: MediaQuery.of(context).size.height * .1,
+            right: 32,
+            child: Text(
+              "Skip",
+              style: TextStyle(fontSize: 16, color: AppColors.textColor),
+            ),
           ),
         ),
         Positioned(
-          left: (SizeConfig.defaultSize ?? 10) * 10, // ✅ تصحيح هنا
-          right: (SizeConfig.defaultSize ?? 10) * 10, // ✅ تصحيح هنا
-          bottom: (SizeConfig.defaultSize ?? 10) * 10, // ✅ تصحيح هنا
+          left: (SizeConfig.defaultSize ?? 10) * 10,
+          right: (SizeConfig.defaultSize ?? 10) * 10,
+          bottom: (SizeConfig.defaultSize ?? 10) * 10,
           child: CustomWidgetButton(
-            text: "Next",
+            text: (pageController?.hasClients ?? false) &&
+                    (pageController?.page?.round() == 2)
+                ? "Get Started"
+                : "Next",
           ),
         ),
       ],
